1e40bf2bbe643dc429d922b60fa069a908fff447
Mohammed Modather <m.modather@ease-group.com>
2022-08-04 13:02:51 +0200

    Finished Fetching and mapping data from weather API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
+import { useState } from 'react';
+import { openWeatherApiConfig } from './api/ApiConfig';
 import CurrentWeather from './components/current-weather/CurrentWeather';
 import Search from './components/search/Search';
 
 function App() {
-  const onSearchCityChange = (city) => {};
+  const [currentWeather, setCurrentWeather] = useState({});
+  const [forecastWeather, SetForecastWeather] = useState({});
+  // ------------------------------------------------------------------->
+  // call api for get weather for pass city
+  const onSearchCityChange = (search) => {
+    const [latitude, longitude] = search.value.split(' ');
+    const currentWeatherFetch = openWeatherApiConfig.get(
+      `weather?lat=${latitude}&lon=${longitude}&appid=${process.env.REACT_APP_OPEN_WEATHER_API_KEY}&units=metric`
+    );
+    const forecastWeatherFetch = openWeatherApiConfig.get(
+      `forecast?lat=${latitude}&lon=${longitude}&appid=${process.env.REACT_APP_OPEN_WEATHER_API_KEY}&units=metric`
+    );
+    Promise.all([currentWeatherFetch, forecastWeatherFetch])
+      .then(async (res) => {
+        setCurrentWeather({ city: search.label, ...res[0]?.data });
+        SetForecastWeather({ city: search.label, ...res[1]?.data });
+      })
+      .catch((err) => {});
+  };
   return (
     <div className='container'>
       <Search onSearchCityChange={onSearchCityChange} />
-      <CurrentWeather />
+      {currentWeather && <CurrentWeather currentWeather={currentWeather} />}
     </div>
   );
 }
--- src/components/current-weather/CurrentWeather.js
+++ src/components/current-weather/CurrentWeather.js
@@ -1,13 +1,20 @@
-export default function CurrentWeather() {
+export default function CurrentWeather({ currentWeather }) {
   return (
     <div className='current-weather'>
       <div className='temperature'>
         <div className='text'>
-          <p className='city'></p>
-          <p className='weather'></p>
+          <p className='city'>{currentWeather?.city}</p>
+          <p className='weather'>
+            {(currentWeather?.weather ?? [])[0]?.description}
+          </p>
         </div>
         <div className='icon'>
-          <img src='assets/images/icons/01d.png' alt='weather-tody' />
+          <img
+            src={`assets/images/icons/${
+              (currentWeather?.weather ?? [])[0]?.icon
+            }.png`}
+            alt='weather-tody'
+          />
         </div>
       </div>
       <div className='details'>
@@ -18,19 +25,21 @@ export default function CurrentWeather() {
           </div>
           <div className='parameter'>
             <span className='label'>Feels Like</span>
-            <span className='value'>22</span>
+            <span className='value'>
+              {Math.round(currentWeather?.main?.feels_like)}°C
+            </span>
           </div>
           <div className='parameter'>
             <span className='label'>Wind</span>
-            <span className='value'>2 m/s</span>
+            <span className='value'>{currentWeather?.wind?.speed} m/s</span>
           </div>
           <div className='parameter'>
             <span className='label'>Humidity</span>
-            <span className='value'>15%</span>
+            <span className='value'>{currentWeather?.main?.humidity}%</span>
           </div>
           <div className='parameter'>
             <span className='label'>Pressure</span>
-            <span className='value'>15 hPa</span>
+            <span className='value'>{currentWeather?.main?.pressure} hPa</span>
           </div>
         </div>
       </div>
