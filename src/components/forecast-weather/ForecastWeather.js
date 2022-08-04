cf6273a131073467145d896a7d8c834ac385b5f4
Mohammed Modather <m.modather@ease-group.com>
2022-08-04 15:21:40 +0200

    Finished Building weather forecast component

---
+++ src/components/forecast-weather/ForecastWeather.js
@@ -0,0 +1,78 @@
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemButton,
+  AccordionItemHeading,
+  AccordionItemPanel,
+} from 'react-accessible-accordion';
+import { DAYS_OF_WEEK } from '../../utils';
+
+export default function ForecastWeather({ forecastWeather }) {
+  const dayInWeek = new Date().getDay(); // get current day
+  const forecastDays = DAYS_OF_WEEK.slice(dayInWeek).concat(
+    DAYS_OF_WEEK.slice(0, dayInWeek)
+  ); // ------------------------------------------------------------------->
+  return (
+    <div className='forecast-weather'>
+      <h2 className='title'>Daily</h2>
+      <Accordion allowZeroExpanded>
+        {forecastWeather?.list?.slice(0, 7).map((item, index) => (
+          <AccordionItem key={index}>
+            <AccordionItemHeading>
+              <AccordionItemButton>
+                <div className='daily-item'>
+                  <img
+                    className='icon-small'
+                    src={`assets/images/icons/${
+                      (item?.weather ?? [])[0]?.icon
+                    }.png`}
+                    alt='weather'
+                  />
+                  <h3 className='day'>{forecastDays[index]}</h3>
+                  <p className='description'>
+                    {item?.weather[0]?.description}
+                    <span className='min'>
+                      {Math.round(item?.main?.temp_min)}°C
+                    </span>{' '}
+                    /
+                    <span className='max'>
+                      {Math.round(item?.main?.temp_max)}°C
+                    </span>
+                  </p>
+                </div>
+              </AccordionItemButton>
+            </AccordionItemHeading>
+            <AccordionItemPanel>
+              <div className='daily-details'>
+                <div className='item'>
+                  <h3 className='title'>Pressure:</h3>
+                  <p className='desc'>{item?.main?.pressure} hPa</p>
+                </div>
+                <div className='item'>
+                  <h3 className='title'>Humidity:</h3>
+                  <p className='desc'>{item?.main?.humidity} %</p>
+                </div>
+                <div className='item'>
+                  <h3 className='title'>Clouds:</h3>
+                  <p className='desc'>{item?.clouds?.all} %</p>
+                </div>
+                <div className='item'>
+                  <h3 className='title'>Wind Speed:</h3>
+                  <p className='desc'>{item?.wind?.speed} m/s</p>
+                </div>
+                <div className='item'>
+                  <h3 className='title'>Sea level:</h3>
+                  <p className='desc'>{item?.main?.sea_level} m</p>
+                </div>
+                <div className='item'>
+                  <h3 className='title'>Feels Like:</h3>
+                  <p className='desc'>{item?.main?.feels_like} °C</p>
+                </div>
+              </div>
+            </AccordionItemPanel>
+          </AccordionItem>
+        ))}
+      </Accordion>
+    </div>
+  );
+}
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,2 +1,13 @@
 // min people population in cities
 export const MIN_POPULATION = 1000000;
+// ------------------------------------------------------------------->
+// days of a week
+export const DAYS_OF_WEEK = [
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+  'Sunday',
+];
